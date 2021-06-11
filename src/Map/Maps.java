45c5c4dceb9d6e1e6eb107eaa7567f152200eb61
Pinky <pinkyabras101@gmail.com>
2021-06-11 23:31:13 +0530

    map and hashmap

---
+++ src/Map/Maps.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Map;
+import java.util.*;  
+import java.util.HashMap;
+/**
+ *
+ * @author asus
+ */
+public class Maps {
+    
+    public static void main(String[] args) {
+      
+        Map<String, Integer> numbers;
+        numbers = new HashMap<>();
+
+        numbers.put("One", 1);
+        numbers.put("Two", 2);
+        System.out.println("Map: " + numbers);
+
+        System.out.println("Keys: " + numbers.keySet());
+
+ 
+        System.out.println("Values: " + numbers.values());
+
+        System.out.println("Entries: " + numbers.entrySet());
+
+        int value = numbers.remove("Two");
+        System.out.println("Removed Value: " + value);
+    }
+}
+
